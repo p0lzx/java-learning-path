c442a040eb31374a31864ee75dc5d121dca4949b
p0lzx <badacad30@gmail.com>
2026-03-21 18:42:23 +0000

    test

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    IO.println("Hello and welcome!");
+
+    for (int i = 1; i <= 5; i++) {
+        IO.println("i = " + i);
+    }
+}
