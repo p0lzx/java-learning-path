9d83eb5dfc0f6ffb2b6800e3444a3608fc980642
p0lzx <badacad30@gmail.com>
2026-03-25 14:43:44 +0100

    Abstract classes and interfaces

---
+++ src/Class/reinforcement2.java
@@ -0,0 +1,13 @@
+package Class;
+
+// final is used in a class to prevent heritage
+// abstract class cannot be instanced (i.e. we cannot create ab object from that class)
+// if we have an abstract method in a class, the class must be abstract
+// A class can either be final or abstract
+public class reinforcement2 {
+}
+
+// interfaces are contract for what classes can do, but not in which way the class must do it
+// an abstract class can have instance variables (objects...) and implements them
+// interface can only instantiate the constants (static final) and [...] ?
+interface reinf2{}
